d01964c05738db5c094cd6b32763057f8136b77e
webapproot <webapproot@gmail.com>
2017-01-15 00:27:34 +0800

    Update linux-update.sh

--- linux-update.sh
+++ linux-update.sh
@@ -11,7 +11,6 @@ Limits(){
         cp /etc/security/limits.conf /root/
         echo "* soft nofile 51200" >>/etc/security/limits.conf
         echo "* hard nofile 51200" >>/etc/security/limits.conf
-
 }
 
 
@@ -20,6 +19,16 @@ Sysctl(){
         wget -O /etc/sysctl.conf https://raw.githubusercontent.com/webapproot/f-/master/sysctl.conf
 }
 
+Serverspeeder(){
+        wget -N --no-check-certificate https://raw.githubusercontent.com/webapproot/Code_shell/master/serverspeeder-all.sh && bash serverspeeder-all.sh
+
+}
+
+
+
+Bbr(){
+        wget --no-check-certificate https://github.com/teddysun/across/raw/master/bbr.sh
+}
 
 Limits
 ulimit -n 51200
@@ -35,4 +44,16 @@ echo $PLAIN
 echo "#####################################################################"
 echo "#####################################################################"
 echo "#####################################################################"
-wget -N --no-check-certificate https://raw.githubusercontent.com/webapproot/Code_shell/master/serverspeeder-all.sh && bash serverspeeder-all.sh
+Serverspeeder
+
+echo "###############################锐速安装一键包 -ok#################################"
+echo "#####################################################################"
+echo "#####################################################################"
+#wget --no-check-certificate https://github.com/teddysun/across/raw/master/bbr.sh
+
+Bbr
+chmod +x bbr.sh
+./bbr.sh
+echo "###############################一键安装最新内核并开启 BBR 脚本 OK #################################"
+echo "#####################################################################"
+echo "#####################################################################"
